e7ff401e55387956e102de55459ca4e95015741d
pc <pc@pc-PC>
2018-11-12 22:05:09 -0500

    Adelanto de login template app

--- application/views/template_app.php
+++ application/views/template_app.php
@@ -1,33 +1,46 @@
-    <!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="es">
     <head>
-           <title><?php echo $this->template->getTitle(); ?></title>   
+        <title><?php echo $this->template->getTitle(); ?></title>   
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">	
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-
         <!--Css generales para login-->
-        <link rel="stylesheet" href="<?php echo base_url() ?>public/css/style-general-login.css">
-        
-
+        <link rel="stylesheet" href="<?php echo base_url() ?>public/app/css/style-general-login.css">
         <!-- iconos con fontawsome-->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
+        <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+        <script>
+            var base_url = '<?php echo base_url(); ?>';
+        </script>
 
+    </head>
+    <body>
+        <?php echo $header; ?>  
+        <?php echo $content; ?>
+        <?php echo $footer; ?>
 
-        <!-- jQuery library -->
+        <!-- Modal -->
+        <div class="modal fade" id="modal_info" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modal_title"></h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body" id="modal_body"></div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-        <!-- Popper JS -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-
-        <!-- Latest compiled JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-
-</head>
-<body>
-  <?php echo $header; ?>  
-  <?php echo $content; ?>
-  <?php echo $footer; ?>
-</body>
+        <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url(); ?>public/app/js/app.js"></script>
+    </body>
 </html>
